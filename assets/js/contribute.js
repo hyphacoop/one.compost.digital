d8678ff2ba1d450e4e9aab71811ceb5178f4429b
Benny Lichtner <bennlich@gmail.com>
2021-01-15 11:10:55 -0800

    js asset test

---
+++ assets/js/contribute.js
@@ -0,0 +1,8 @@
+console.log('YO ITS CONTRIBUTE TIME')
+
+function render() {
+  console.log(document.querySelector('[amount-raised]'))
+  document.querySelector('[amount-raised]').innerText = 'dogs'
+}
+
+render()
