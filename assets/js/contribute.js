715a975abe93c9290be955f332a21633b32c77a0
Benny Lichtner <bennlich@gmail.com>
2021-01-21 22:19:45 -0800

    fetch from production monetization api; set protocol depending on browser protocol

--- assets/js/contribute.js
+++ assets/js/contribute.js
@@ -1,8 +1,21 @@
-console.log('YO ITS CONTRIBUTE TIME')
+let protocol = 'https:'
+if (window.location.protocol === 'ipfs:' || window.location.protocol === 'ipns:') {
+  protocol = 'ipns:'
+} else if (window.location.protocol === 'hyper:') {
+  protocol = 'hyper:'
+}
+let monetizationUrl = `${protocol}//api.compost.digital/v0/monetization/balances.json`
 
-function render() {
-  console.log(document.querySelector('[amount-raised]'))
-  document.querySelector('[amount-raised]').innerText = 'dogs'
+async function render() {
+  console.log('Fetching balances...')
+  let balances
+  try {
+    balances = await fetch(monetizationUrl).then(res => res.json())
+  } catch (e) {
+    console.error(e)
+  }
+  console.log(balances)
+  document.querySelector('[amount-raised]').innerText = JSON.stringify(balances, null, 2)
 }
 
 render()
