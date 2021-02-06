1a9f5881b7c8793ba18e47ae4b27fa7374d3afec
Benny Lichtner <bennlich@gmail.com>
2021-02-06 08:56:43 -0800

    start table of contents

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function showTableOfContents() {
+  document.querySelector('[table-of-contents-container]').style.display = 'block'
+}
+
+function hideTableOfContents() {
+ document.querySelector('[table-of-contents-container]').style.display = 'none' 
+}
