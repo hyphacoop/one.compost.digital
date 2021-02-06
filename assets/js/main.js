61d17d9b3297c440f07280c3428e94dda410cc3a
Benny Lichtner <bennlich@gmail.com>
2021-02-06 11:40:23 -0800

    iterating on that footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 function showTableOfContents() {
   document.querySelector('[table-of-contents-container]').style.display = 'block'
+  document.body.classList.add('no-scroll')
 }
 
 function hideTableOfContents() {
- document.querySelector('[table-of-contents-container]').style.display = 'none' 
+ document.querySelector('[table-of-contents-container]').style.display = 'none'
+ document.body.classList.remove('no-scroll')
 }
