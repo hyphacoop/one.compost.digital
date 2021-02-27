fe279918d2a70b90b209082804253bb8fac20cba
Benedict Lau <b@deprecated.systems>
2021-02-27 17:26:59 -0500

    Add mobile version of shafai art

--- assets/js/shafali.js
+++ assets/js/shafali.js
@@ -10,6 +10,22 @@ resizeIframes()
 
 window.addEventListener('resize', resizeIframes)
 
+if (isDesktop()) {
+  document.querySelector('#biasesF').src = "../../shafali/biasesF_2021_01_19_17_20_19/"
+  document.querySelector('#materialF').src = "../../shafali/materialF_2021_01_19_17_20_25/"
+  document.querySelector('#CreatorF').src = "../../shafali/CreatorF_2021_01_19_17_20_32/"
+  document.querySelector('#LiteracyF').src = "../../shafali/LiteracyF_2021_01_19_17_20_37/"
+  document.querySelector('#RespectF').src = "../../shafali/RespectF_2021_01_19_17_20_12/"
+  document.querySelector('#IGF').src = "../../shafali/intergenerational_Final_2021_01_19_17_19_49/"
+} else {
+  document.querySelector('#biasesF').src = "../../shafali/biasesF_2021_01_19_17_20_19_mobile/"
+  document.querySelector('#materialF').src = "../../shafali/materialF_2021_01_19_17_20_25_mobile/"
+  document.querySelector('#CreatorF').src = "../../shafali/CreatorF_2021_01_19_17_20_32_mobile/"
+  document.querySelector('#LiteracyF').src = "../../shafali/LiteracyF_2021_01_19_17_20_37_mobile/"
+  document.querySelector('#RespectF').src = "../../shafali/RespectF_2021_01_19_17_20_12_mobile/"
+  document.querySelector('#IGF').src = "../../shafali/intergenerational_Final_2021_01_19_17_19_49_mobile/"
+}
+
 // document.querySelectorAll('iframe').forEach(el => 
 //   el.addEventListener('touchstart', (e) => e.preventDefault())
 //   el.addEventListener('touchmove', (e) => e.preventDefault())
