04ba1897aa20ec3b3abedfa7abfc8106596a4361
Benny Lichtner <bennlich@gmail.com>
2021-02-27 23:40:12 -0800

    next pass at micahs piece; annotations 1-4 are complete

--- assets/js/micah.js
+++ assets/js/micah.js
@@ -1,11 +1,7 @@
 console.log('micah')
 
 async function init() {
-  let annotations = await fetch(micahAnnotationURL).then(res => res.json())
-
-  console.log(annotations)
-  
-  var viewer = OpenSeadragon({
+  var viewer = window.viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: `/js/vendor/openseadragon-bin-2.4.2/images/`,
     tileSources: {
@@ -33,17 +29,52 @@ async function init() {
     }
   ]
 
-  markers.forEach(marker => {
-    const onMarkerClick = (e) => {
-      console.log(marker)
-      e.preventDefault()
-      e.stopPropagation()
+  let annotations = await fetch('../../micah/micahsMapAnnotations.json').then(res => res.json())
+  console.log(annotations)
+  
+  let infoEl = html`<div id="mesh-map-info-container"></div>`
+  document.querySelector('.openseadragon-container').appendChild(infoEl)
+
+  const renderInfo = (annotation) => {
+    const makeMediaElement = (item) => {
+      if (item.type === 'image') {
+        return html`<img src="../../${item.url}">`
+      } else if (item.type === 'audio') {
+        return html`<audio controls src="../../${item.url}">`
+      }
+    }
+    
+    infoEl.innerHTML = ''
+
+    if (annotation) {
+      let contentEl = html`
+        <div class="mesh-map-info-content" onclick=${(e) => e.stopPropagation()}>
+          ${annotation.media.map(item => makeMediaElement(item))}
+          <div class="text">${annotation.text}</div>
+        </div>
+      `
+      infoEl.appendChild(contentEl)
+    }
+  }
+
+  // let width = 4960
+  // let height = 3508
+  let width = 1920
+  let height = 1358
+
+  annotations.forEach(annotation => {
+    let marker = {
+      id: annotation.id,
+      x: annotation.x / width,
+      y: 0.7 * annotation.y / height,
+      placement: 'CENTER',
+      checkResize: false
     }
     
     let markerElement = html`<img
       class="mesh-marker"
       id="right-arrow-overlay"
-      src="http://upload.wikimedia.org/wikipedia/commons/7/7a/Red_Arrow_Right.svg"
+      src="../../micah/info-icon.svg"
       alt="Right arrow"
       width="20">`
 
@@ -52,12 +83,27 @@ async function init() {
       element: markerElement,
       clickHandler: function(event) {
         console.log(marker)
+        renderInfo(annotation)
         event.preventDefaultAction = true
       }
     })
 
     marker.element = markerElement
     viewer.addOverlay(marker)
+  })
+
+  viewer.addHandler('canvas-click', (event) => {
+    if (infoEl.children.length > 0) {
+      renderInfo(null)
+      event.preventDefaultAction = true  
+    }
+  })
+
+  document.addEventListener('click', (event) => {
+    console.log('document click', event.target)
+    if (!event.target.classList.contains('mesh-marker')) {
+      renderInfo(null)
+    }
   })
 }
 
--- content/pieces/fertile-technofutures-from-bidar/index.md
+++ content/pieces/fertile-technofutures-from-bidar/index.md
@@ -8,4 +8,25 @@ endingImageUrl: ./images/angelica-gifs/mantis_computer.gif
 layout: micah
 ---
 
+## Introduction and context setting
+
+This piece is a group reflection of a project run in Bidar, India, to set up a community mesh network facilitated by Living Labs Network and Forum, and Janastu/Servelots. 
+
+This local mesh network was to act as an intranet of local informal archives that would collectively function as a knowledge network. These archives come from various communities in Bidar and their knowledge practices. During the course of the project, we came to see both the limitations of technology but also the potential of it to catalyse and nurture many more spaces of cultural collaboration. 
+
+We would like to present narratives from the field of possibilities and futures that our network members came across. The main aim of this endeavour was to set up the network as a way to to document and creatively engage with folklore, songs, and oral practices held by women of various communities in Bidar. The intent was to connect the unconnected through collaborative activities and create mutual annotation of oral knowledge practices. 
+
+## How to read this artifact
+
+The following image has been annotated with descriptions of the projects, responses from various network members, and internal thoughts and reflections. The image is a condensed map of places, artifacts and people in these different spaces. It is an exercise in sensemaking through collecting different experiences, emotions, epiphanies and hopes in the context of the mesh. It is an abstraction of Bidar  — a small city with rich histories of diverse populations and cultures meeting.
+
+## Why is it made this way 
+
+It is a common gripe among researchers and activists in our area of work in India, that writing in English forces us to capture and categorise. This is a smaller effort on our part to bring out the intangible, give a glimpse into the cultural interconnections as well as account for different articulations than what a traditional format allows. During this piece, you might get lost and some things won’t make sense and you’d want to know things that aren’t explicitly said. But imagine yourself as a traveler. Please wonder and wander as you explore and listen. 
+
+## Techno-Futures from Bidar
+
+The following image has been annotated with descriptions of the projects. Please click around the map to experience responses, reflections, and songs from network members in Bidar.
+
+
 <!-- see layouts/pieces/micah.html -->
